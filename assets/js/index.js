3a2447e57318aae0dddf5c0a1e885d7717190b21
harshana-lk <harshanajayaweerayt@gmail.com>
2023-05-08 19:48:59 +0530

    added click evt & keypress evt

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".button");
+document.getElementById("display").focus();
 
+buttons.forEach((itm) => {
+  itm.addEventListener("click", "keypress", (e) => {
+    switch (itm.value) {
+      case "AC":
+        display.value = "";
+        break;
+      case "DE" || "Backspace":
+        display.value = display.value.slice(0, -1);
+        break;
+      case "=" || "Enter":
+        try {
+          display.value = eval(display.value);
+        } catch {
+          alert("Error Calculation!");
+          display.value = "";
+        }
+        break;
+      default:
+        display.value += itm.value;
+    }
+  });
+});
